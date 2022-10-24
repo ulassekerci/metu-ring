4e16f04518c21b92609977829f206bf6b1c9ff91
ulassekerci <ulassekerci22@gmail.com>
2022-10-24 21:14:11 +0300

    fixed react/no-unescaped-entities

--- components/DataBar.tsx
+++ components/DataBar.tsx
@@ -4,7 +4,7 @@ const DataBar: FC<{ status: number }> = ({ status }) => {
   return (
     <div className='absolute bottom-0 left-0 h-12 w-full bg-red-600 grid place-content-center z-10'>
       {status === 2 && <span className='text-white'>There are no ring busses</span>}
-      {status === 3 && <span className='text-white'>Couldn't get data</span>}
+      {status === 3 && <span className='text-white'>Could not get data</span>}
     </div>
   )
 }
