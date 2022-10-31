24b5d4b878bf951e84725d0c7c45e12d0952dc57
Ulaş <107992457+ulassekerci@users.noreply.github.com>
2022-10-31 23:32:52 +0300

    made text easier to understand

--- components/DataBar.tsx
+++ components/DataBar.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react'
 const DataBar: FC<{ status: number }> = ({ status }) => {
   return (
     <div className='absolute bottom-0 left-0 h-12 w-full bg-red-600 grid place-content-center z-10'>
-      {status === 2 && <span className='text-white'>There are no ring busses</span>}
+      {status === 2 && <span className='text-white'>There are no active ring busses right now</span>}
       {status === 3 && <span className='text-white'>Could not get data</span>}
     </div>
   )
